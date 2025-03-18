98a41e359553057206de329a95481cea6aa5c3ec
aktapaz <christoph.zapatka@ingrammicro.com>
2025-03-18 11:18:01 +0100

    playing around...

---
+++ RetailPriceWebApp/Program.cs
@@ -0,0 +1,26 @@
+//// filepath: RetailPriceWebApp/Program.cs
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
