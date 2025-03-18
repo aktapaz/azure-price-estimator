820453a6bc764bb7b066f2a5b886c9c9876f331e
aktapaz <christoph.zapatka@ingrammicro.com>
2025-03-18 14:41:53 +0000

    adding "publish" folder

--- RetailPriceWebApp/obj/Release/net8.0/RetailPriceWebApp.AssemblyInfo.cs
+++ RetailPriceWebApp/obj/Release/net8.0/RetailPriceWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290f0be4664a14e2b404db5ff49589d901d183dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1ac6d2e8831ca76e5e7cbe97e46e01e9d5f40cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
