628b998e74f0b0bb6f73489928c056495a5b6a1a
aktapaz <christoph.zapatka@ingrammicro.com>
2025-03-25 12:19:56 +0100

    added .net extension

--- RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
+++ RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56c4bb1d0eaa98aa765fe08beabf78a2c9039f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde5b459a0d509089b8b6e3e6c507a75593c5f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
