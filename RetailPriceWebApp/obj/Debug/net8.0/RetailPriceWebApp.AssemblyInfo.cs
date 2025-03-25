dd051626909e702719982c67326690dbc5262b5a
aktapaz <christoph.zapatka@ingrammicro.com>
2025-03-25 15:22:21 +0100

    dotnet publish

--- RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
+++ RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce20d08b749e34a650b6b579158f1a7316a96cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29b265be0f53aa4838a74f5bdbb913487accefe")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
