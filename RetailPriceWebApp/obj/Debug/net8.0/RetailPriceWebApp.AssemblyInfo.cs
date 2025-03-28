8412209ee8e717bede92d3177f90dced5579947a
aktapaz <christoph.zapatka@ingrammicro.com>
2025-03-28 13:56:06 +0100

    Build errors fixed for now

--- RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
+++ RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93f043f5c58a360011400699bb9db79e1177731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03ac0ccc83c0db7895dbf7c58fe0df10b7d48e1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
