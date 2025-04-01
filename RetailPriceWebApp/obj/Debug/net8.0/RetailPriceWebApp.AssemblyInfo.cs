8f3ae6cd6c41f6e04049d72eec7a9fdfc9dc71a9
aktapaz <christoph.zapatka@ingrammicro.com>
2025-04-01 17:36:39 +0200

    added ri pricing

--- RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
+++ RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f163a9a4857452a47646830de76abd77454850c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79771b13ab6f69490ee0d7de2cf281edba659d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
