ac00fd44f3e4fa5110dc4471d3471668a96c3d1e
aktapaz <christoph.zapatka@ingrammicro.com>
2025-04-01 09:39:36 +0200

    storage options added and new build

--- RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
+++ RetailPriceWebApp/obj/Debug/net8.0/RetailPriceWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae52bc7105bce2b821eaf5ce6caf0a82a3f4364")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f64f0995e79150a067516b082e95863d7c9a9538")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetailPriceWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
